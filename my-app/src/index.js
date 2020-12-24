bf407abae998e6299e1113a2807db02be0c181b4
Balint Kozak <kozak.balint15@gmail.com>
2020-12-24 14:07:35 +0100

    Time travel.

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -50,12 +50,13 @@ function Square(props) {
             history: [{
                 squares: Array(9).fill(null),
             }],
+            stepNumber: 0,
             xIsNext: true,
         }
     }
 
     handleClick(i) {
-        const history = this.state.history;
+        const history = this.state.history.slice(0, this.state.stepNumber+ 1);
         const current = history[history.length - 1];
         const squares = current.squares.slice();
         if (checkWinner(squares) || squares[i]) {
@@ -66,13 +67,21 @@ function Square(props) {
             history: history.concat([{
                 squares: squares,
             }]),
+            stepNumber: history.length,
             xIsNext: !this.state.xIsNext,
+        });
+    }
+
+    jumpTo(step) {
+        this.setState({
+            stepNumber: step,
+            xIsNext: (step % 2) === 0,
         });
     }
 
     render() {
         const history = this.state.history;
-        const current = history[history.length-1];
+        const current = history[this.state.stepNumber];
         const winner = checkWinner(current.squares);
 
         const moves = history.map((step, move) => {
